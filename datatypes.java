f3a58da5e329d08fab567da07e9a532871aaa99c
Tanzil <tanzilibrahim5@gmail.com>
2025-07-19 18:14:25 +0530

    update

--- datatypes.java
+++ datatypes.java
@@ -1,3 +1,21 @@
 public class datatypes {
+    public static void main(String[] args) {
+        boolean flag = true;
+        int a = 12345;
+        byte b = 120;
+        short s = 1200;
+        long l = 832786310;
+        float f = 800.7692f;
+        double d = 7878.876;
+        char ch = 'p';
+        System.out.println("The boolean variable is " + flag);
+        System.out.println("The integer variable is " + a);
+        System.out.println("The byte variable is " + b);
+        System.out.println("The short variable is " + s);
+        System.out.println("The long variable is " + l);
+        System.out.println("The float variable is " + f);
+        System.out.println("The double variable is " + d);
+        System.out.println("The char variable is " + ch);
+    }
     
 }
