70accf69e9cd06038c2b90a1daf94009ebef7728
Tanzil <tanzilibrahim5@gmail.com>
2025-07-19 15:44:39 +0530

    Update

--- learn.java
+++ learn.java
@@ -2,13 +2,33 @@
 //object is  instances of a class
 class learn{
     public static void main(String[] args) { // function
+        int money = 2000;
+        //int -integer
 // void - return type. main - function name or entry point in a java program
 // public - access modifier. static - it allows to call this method without creating an object of the class
 // String[] args - command line arguments 
 // [] - list
-System.out.println("Hello world java");
+System.out.println(money);
+money = 5000;
+System.out.println(money);
 // ln - next line
 // system - class  out - object of print stream println - method in print stream
-//Java varibles - titles of reserved memory location.
+//Java varibles - titles of reserved memory location.A variable in Java is like a container or box where you can store a value
+//strings -collection of characters
+//camelcase - first word - small letters second word - First letter in capital
+//functions.variables - in camelCase
+//Pascalcase - first letter should be capital in all word
+//class - in Pascalcase
+//packages - in lower case
+//snake case - separate two or more words by underscore(_)
+//constant - should not be change. in upper snake case
+//Rules for naming variables in java
+   //Rule 1 - variables names should not begin with a number (e.g.-num_1)
+   //Rule 2 - whitespace is not permitted in variabale names (e.g. - int money_Borrowed)
+   //Rule 3 - a java keyword (reserved word) cannot be used as a variable name
+   //Rule 4 - it is preferred to use variable names with more than one word with all lowercase letters for the first word and capitalization of the first letter of each subsequent word
+   //Rule 5 - when creating variables, it is preferred to give them meaningful names
+   //Rule 6 - all lowercase letters should be used when creating one-word varibles names
+//Java Identifiers - 
     }
 }
