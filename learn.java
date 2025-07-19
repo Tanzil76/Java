b02c12610b2a10c63fbe311eedbdd044a90f2c0e
Tanzil <tanzilibrahim5@gmail.com>
2025-07-19 17:39:38 +0530

    update

--- learn.java
+++ learn.java
@@ -23,12 +23,17 @@ System.out.println(money);
 //snake case - separate two or more words by underscore(_)
 //constant - should not be change. in upper snake case
 //Rules for naming variables in java
-   //Rule 1 - variables names should not begin with a number (e.g.-num_1)
+    //Rule 1 - variables names should not begin with a number (e.g.-num_1)
    //Rule 2 - whitespace is not permitted in variabale names (e.g. - int money_Borrowed)
    //Rule 3 - a java keyword (reserved word) cannot be used as a variable name
    //Rule 4 - it is preferred to use variable names with more than one word with all lowercase letters for the first word and capitalization of the first letter of each subsequent word
    //Rule 5 - when creating variables, it is preferred to give them meaningful names
    //Rule 6 - all lowercase letters should be used when creating one-word varibles names
-//Java Identifiers - 
+//Java Identifiers - any name you create in java 
+  //Rule 1- all identifiers should begin with a letter (A to Z or a to z),currency character($) or an underscore(_)
+  //Rule 2- after the first character,identifiers can have any combination of characters
+  //Rule 3- a keyword cannot be used as an identifier
+  //Rule 4- the identifiers are case sensitive
+  //Rule 5- whitespace is not permitted
     }
 }
