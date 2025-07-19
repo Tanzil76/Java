8cc3aa38bd4c7bdd927ad583e2fd77fbd4d64168
Tanzil <tanzilibrahim5@gmail.com>
2025-07-19 13:59:36 +0530

    Initial commit

---
+++ learn.java
@@ -0,0 +1,14 @@
+// class is a user defined blueprint consisting of attributes and methods 
+//object is  instances of a class
+class learn{
+    public static void main(String[] args) { // function
+// void - return type. main - function name or entry point in a java program
+// public - access modifier. static - it allows to call this method without creating an object of the class
+// String[] args - command line arguments 
+// [] - list
+System.out.println("Hello world java");
+// ln - next line
+// system - class  out - object of print stream println - method in print stream
+
+    }
+}
