89f2cc2aef1d9cccda1b11462a322545838dd47a
Tanzil <tanzilibrahim5@gmail.com>
2025-07-20 19:39:38 +0530

    update

---
+++ RelationalOperators.java
@@ -0,0 +1,15 @@
+/* ==(is equals to),!(not equals to),<,>,>=(greater than or equals to),<= */
+
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int p = 10 , q = 15;
+
+        System.out.println(p==q); // false
+        System.out.println(p!=q); // true
+        System.out.println(p<=q); // true
+        System.out.println(p>=q); // false
+        System.out.println(p<q); // true
+        System.out.println(p>q); // false
+
+    }
+}
