c7519c6f0bde0175d19d50c8cbb2811738f5f7c0
Tanzil <tanzilibrahim5@gmail.com>
2025-07-20 19:29:05 +0530

    update

---
+++ ArithmeticOperators.java
@@ -0,0 +1,17 @@
+ /* Arithmetic Operators- +,-,/,*,%,--,++ */
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+
+    int p = 20, q = 10;
+
+    System.out.println(p+q);
+    System.out.println(p-q);
+    System.out.println(p/q);
+    System.out.println(p*q);
+    System.out.println(p%q);
+
+    }
+}
+
+
