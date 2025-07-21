86daf1683edf72bfaf6ae1b6e65bd548dd3fce1a
Tanzil <tanzilibrahim5@gmail.com>
2025-07-21 12:41:30 +0530

    update

---
+++ LogicalOperators.java
@@ -0,0 +1,21 @@
+/* &&(logical AND) , ||(logical OR) , !(logical NOT)  */
+public class LogicalOperators {
+    public static void main(String[] args) {
+        int p = 15 , q = 10 , r = 5;
+
+        // && operator
+        System.out.println((p>q) && (p>r)); //true
+        System.out.println((p>q) && (p<r)); // false
+
+        // || operator(atleast one should be true)
+        System.out.println(( r<q) || (p<q)); // true
+        System.out.println((p>q) || (q>r)); // true
+        System.out.println((p<q) || (q<r)); // false
+
+        // ! operator
+        System.out.println(!(p==q)); // true
+        System.out.println(!(p>q)); // false
+    }
+
+    }
+
