8b69375ca3c0f182b17e0bf47e4d949b279f9db6
Tanzil <tanzilibrahim5@gmail.com>
2025-07-25 19:03:53 +0530

    update

---
+++ whileloop.java
@@ -0,0 +1,19 @@
+/* print the sum of the first n natural numbers, where n is the input */
+import java.util.Scanner;
+
+public class whileloop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+
+        int num = 1;
+
+        while (num <= n) {
+            sum = sum + num;
+            num++;
+            
+        }
+        System.out.println(sum);
+    }
+}
