391b5c0e9b7ad89faba078cbe77357a67baa83c2
Tanzil <tanzilibrahim5@gmail.com>
2025-07-26 14:49:15 +0530

    update

--- forloop.java
+++ forloop.java
@@ -1,4 +1,8 @@
-// forloop
+/* for loop - for loop ek control flow statement hai jo Java mein use hota hai jab tumhe ek code block ko fixed number of times ya ek 
+    specific range mein repeat karna ho.
+    for(init-statement;condition;final-expression){
+    //code
+    } */ 
 
 import java.util.Scanner;
 
@@ -6,9 +10,11 @@ public class forloop {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
+        int sum = 0;
         for(int num = 1; num <= n; num++){
-            System.out.println(num);
+            sum = sum + num;
         }
+            System.out.println("Sum of numbers upto n");
+        
     }
 }
