062fbf3f683e5ef7bfa4b15ed0080f02686d4a6b
Tanzil <tanzilibrahim5@gmail.com>
2025-07-26 15:33:20 +0530

    update

--- dowhileloop.java
+++ dowhileloop.java
@@ -2,13 +2,19 @@
  * do {
  * //code
  * }while(condition);
+ * print the sum of the stream of integers in the input.
 */
 import java.util.Scanner;
 
 public class dowhileloop {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc/nextInt();
-
+        int num = sc.nextInt();
+        int sum = 0;
+        while(num != -1){
+            sum += num;
+            num = sc.nextInt();
+        }
+        System.out.println(sum);
     }
 }
