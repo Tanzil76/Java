0ceb0c2f5a44cf31cfaed25d4776a079e6e9cd37
Tanzil <tanzilibrahim5@gmail.com>
2025-07-27 23:13:46 +0530

    update

--- problemloop3.java
+++ problemloop3.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class problem3 {
+public class problemloop3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
